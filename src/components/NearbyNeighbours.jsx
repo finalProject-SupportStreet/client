a3d11bdac1ef47ad99920aec638957daf89a3a25
Friedemann Denzer <friedemann_denzer@posteo.de>
2024-03-05 10:32:07 +0100

    NearbyNeighbours.jsx done

--- src/components/NearbyNeighbours.jsx
+++ src/components/NearbyNeighbours.jsx
@@ -45,7 +45,7 @@ const NearbyNeighbours = () => {
 
 
   const divStyle = {
-    border: '1px solid black',
+    border: '1px solid darkblue',
     backgroundColor: 'lightblue',
     padding: '1rem',
     marginTop: '1rem'
@@ -53,7 +53,7 @@ const NearbyNeighbours = () => {
   const liStyle = {
     padding: '1rem',
     margin: '0.4rem 0 0.4rem 0',
-    border: '3px solid black',
+    border: '3px solid darkblue',
     borderRadius: '8px'
   };
 
