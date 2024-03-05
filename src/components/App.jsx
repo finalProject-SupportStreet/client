1ba2a477e02b3abeff6280623edabae36015dacf
Oliver <kuehne89@proton.me>
2024-03-05 13:46:20 +0100

    branch probleme lösen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,29 @@
-import { useEffect, useContext, useState } from "react";
-import {  Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import "../App.css";
-import Navbar from "./mainComponents/Navbar.jsx";
+import Nav from "./mainComponents/Navbar.jsx";
 import { useTheme } from "./context/ThemeContext.jsx";
+
 function App() {
   const { darkMode, toggleDarkMode } = useTheme();
 
+  /*   localStorage.removeItem("theme"); // Whenever the user explicitly chooses to respect the OS preference */
 
-
-  
-/*   localStorage.removeItem("theme"); // Whenever the user explicitly chooses to respect the OS preference */
-  
-  
-
-
-
-{/* <div className={`h-full  px-0 box-content ${darkMode ? 'dark' : ''}`}> */}
-return (
-  <div>
-      
-      <Nav></Nav>
-    <div className={`h-screen flex items-center justify-center px-4 w-full ${darkMode ? 'dark darkThemeColor' : ''}`}>
-    
-      <Outlet />
+  {
+    /* <div className={`h-full  px-0 box-content ${darkMode ? 'dark' : ''}`}> */
+  }
+  return (
+    <div>
+      <Nav />
+      <div
+        className={`h-screen flex items-center justify-center px-4 w-full ${
+          darkMode ? "dark darkThemeColor" : ""
+        }`}
+      >
+        <Outlet />
+      </div>
     </div>
-  </div>
-);
+  );
 }
-
 
 export default App;
