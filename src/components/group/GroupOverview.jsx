2b0cde79b5af170b2d4f66a540fdf9f1001c5d00
Oliver <kuehne89@proton.me>
2024-03-12 09:50:10 +0100

    groupCard, searchBtn, und register/login nach merge repariert

--- src/components/group/GroupOverview.jsx
+++ src/components/group/GroupOverview.jsx
@@ -1,14 +1,24 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../context/userContext.jsx";
 import GroupCard from "./GroupCard.jsx";
 import { GroupsContext } from "../context/groupsContext.jsx";
+import Searchbar from "./Searchbar.jsx";
+import SearchBtnUnclick from "./SearchBtnUnclick.jsx";
 
 const GroupOverview = () => {
   const { userData } = useContext(UserContext);
   const { groupsData, setGroupsData } = useContext(GroupsContext);
-  console.log("userData in Overview", userData);
+  const [searchInputVisible, setSearchInputVisible] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [dropDFilterIsOpen, setDropDFilterIsOpen] = useState(false);
 
-  // Groupsfetch und daten in den Context laden
+  console.log("Userdata Groups in overwiev:", userData.groups);
+  /******************************************************
+   *    Groupsfetch und daten in den Context laden
+   ******************************************************/
+  //^ am ende mal schauen ob der GruppenContext notwendig ist
+
   useEffect(() => {
     const fetchGroupsData = async () => {
       try {
@@ -26,68 +36,169 @@ const GroupOverview = () => {
     fetchGroupsData();
   }, []);
 
-  console.log("groups Fetch in overview", groupsData);
+  /******************************************************
+   *    SuchFunktion
+   ******************************************************/
+
+  useEffect(() => {
+    console.log(searchResults);
+  }, [searchResults]);
+
+  // Funktion zum Umschalten der Sichtbarkeit des Suchfelds
+  const toggleSearchInput = () => {
+    setSearchInputVisible(!searchInputVisible);
+  };
+
+  // Funktion zum Ausführen der Suche
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `http://localhost:5500/getSearchGroups/${searchValue}`,
+        {
+          credentials: "include",
+        }
+      );
+
+      const responseData = await response.json(); // in JSON umgewandeln
+
+      //! Error Message durch Userverständliche MSG ersetzten
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      setSearchResults(responseData); // Suchergebnisse basierend auf den JSON-Daten setzten
+
+      console.log("SEARCH-FETCH (searchResults) ERGEBNIS: ", searchResults);
+    } catch (error) {
+      console.error("Error searching groups:", error);
+    }
+    // Sichtbarkeit des Suchfelds zurück setzen
+    setSearchInputVisible(false);
+    setSearchValue("");
+  };
+
+  /******************************************************
+   *    FilterFunktion
+   ******************************************************/
+  const toggleDropdown = () => {
+    setDropDFilterIsOpen(!dropDFilterIsOpen);
+  };
 
   return (
     <>
-      <div className="m-5 ">
+      <div className="mt-12">
         <div className="min-w-420px">
           <div className="groupBar bg-stone-400 mt-2 border border-black">
-            <header className="p-5 ">
+            <header className="p-5">
               <h2 className="text-3xl">Gruppen</h2>
             </header>
             <div className="groupHeaderBar flex justify-between items-center border border-black p-2">
               <ul className="border-black flex justify-between w-80">
-                <li className="flex items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  Suchen
-                </li>
-                <li className="flex items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"
-                    />
-                  </svg>
-                  Filter
-                </li>
-                <li className="flex items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0-3.75-3.75M17.25 21 21 17.25"
-                    />
-                  </svg>
-                  Sortieren
-                </li>
+                {searchInputVisible ? (
+                  <Searchbar
+                    toggleSearchInput={toggleSearchInput}
+                    searchValue={searchValue}
+                    setSearchValue={setSearchValue}
+                    handleSearch={handleSearch}
+                  />
+                ) : (
+                  <SearchBtnUnclick toggleSearchInput={toggleSearchInput} />
+                )}
+                {!searchInputVisible && (
+                  <>
+                    <li className="flex items-center" onClick={toggleDropdown}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"
+                        />
+                      </svg>
+                      Filter
+                    </li>
+                    {dropDFilterIsOpen && (
+                      <select
+                        id="tags"
+                        name="tags"
+                        className="mt-1 block text-gray-800 w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+                      >
+                        <option value="" disabled>
+                          Wähle eine Kategorie aus...
+                        </option>
+                        <option value="Kennlern/Stammtisch">
+                          Kennlern/Stammtisch
+                        </option>
+                        <option value="Bildung/Erfahrung">
+                          Bildung/Erfahrung
+                        </option>
+                        <option value="Kunst, Kultur & Musik">
+                          Kunst, Kultur & Musik
+                        </option>
+                        <option value="Märkte & Flohmärkte">
+                          Märkte & Flohmärkte
+                        </option>
+                        <option value="Computer, Internet & Technik">
+                          Computer, Internet & Technik
+                        </option>
+                        <option value="Familien & Kinder">
+                          Familien & Kinder
+                        </option>
+                        <option value="Essen & Trinken">Essen & Trinken</option>
+                        <option value="Feste & Feiern">Feste & Feiern</option>
+                        <option value="Lokales Engagement">
+                          Lokales Engagement
+                        </option>
+                        <option value="Gestalten & Heimwerken">
+                          Gestalten & Heimwerken
+                        </option>
+                        <option value="Gesundheit / Wellness">
+                          Gesundheit / Wellness
+                        </option>
+                        <option value="Sport & Bewegung">
+                          Sport & Bewegung
+                        </option>
+                        <option value="Umwelt & Nachhaltigkeit">
+                          Umwelt & Nachhaltigkeit
+                        </option>
+                        <option value="Teilen, Tauschen, Reparieren">
+                          Teilen, Tauschen, Reparieren
+                        </option>
+                        <option value="Viertel verschönern">
+                          Viertel verschönern
+                        </option>
+                        <option value="Ausflüge">Ausflüge</option>
+                        <option value="Sonstiges">Sonstiges</option>
+                      </select>
+                    )}
+                    <li className="flex items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0-3.75-3.75M17.25 21 21 17.25"
+                        />
+                      </svg>
+                      Sortieren
+                    </li>
+                  </>
+                )}
               </ul>
+
               <aside className="pr-3">
                 <a href="http://localhost:5173/groupsForm">
                   <svg
@@ -108,21 +219,40 @@ const GroupOverview = () => {
           </div>
 
           {/* VORLAGE: MUSS ANGEPASST WERDEN! */}
-          <span>Deine Gruppen</span>
-          <ul className="h-full">
-            {groupsData &&
-              groupsData.map((group) => (
-                <GroupCard key={group.title} group={group} />
-              ))}
-          </ul>
+          {searchResults[0] ? (
+            <>
+              <span>gefundene Suchergebnisse</span>
+              <button
+                className="border border-solid ml-5"
+                onClick={() => setSearchResults([])}
+              >
+                Zurück
+              </button>
+              <ul className="h-auto">
+                {searchResults.map((group, index) => (
+                  <GroupCard key={index} group={group} />
+                ))}
+              </ul>
+            </>
+          ) : (
+            <>
+              <span>Deine Gruppen</span>
+              <ul className="h-auto">
+                {userData.groups &&
+                  userData.groups.map((group, index) => (
+                    <GroupCard key={index} group={group} />
+                  ))}
+              </ul>
 
-          <span>Andere Gruppen</span>
-          <ul className="h-auto">
-            {groupsData &&
-              groupsData.map((group) => (
-                <GroupCard key={group.title} group={group} />
-              ))}
-          </ul>
+              <span>Andere Gruppen</span>
+              <ul className="h-auto">
+                {groupsData &&
+                  groupsData.map((group, index) => (
+                    <GroupCard key={index} group={group} />
+                  ))}
+              </ul>
+            </>
+          )}
         </div>
       </div>
     </>
