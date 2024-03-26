0ac277b381dbfc5484b270fb5c45891e2b0be295
RedFox170 <124230777+RedFox170@users.noreply.github.com>
2024-03-26 10:20:32 +0100

    Oli finalbranch (#21)
    * branch probleme lösen
    
    * 15:30
    
    * groupCard, searchBtn, und register/login nach merge repariert
    
    * aktuellen stand commiten/ doppelte datein löschen
    
    * aktuellen stand commiten/ doppelte datein löschen
    
    * create Post MARKT, MITTEILUNG! Form geht, fetch fehlt
    
    * einbindung von Markt.jsx und marktForm
    
    * group form bugs behoben
    
    * CreateGroupPost zum laufen gekriegt
    
    * fehlerhafter Stand undefined URL
    
    * push
    
    * styling, Kommentar funktion repariert, Bug fixing
    
    * gruppen styling so gut wie fertig
    
    * neue Gruppen anlegen Bug behoben+ landingPage
    
    * push
    
    * merge conflicts
    
    * dev-stop

--- src/components/mainComponents/DropDownProfile.jsx
+++ src/components/mainComponents/DropDownProfile.jsx
@@ -1,9 +1,7 @@
 import { useRef, useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-import "../../../src/App.css";
-import UserLogout from "../user/UserLogout.jsx";
 
-// import { buttonStyle } from "../reuseable/styles/reuseableComponents.jsx";
+import UserLogout from "../user/UserLogout.jsx";
 
 export const DropDownProfile = () => {
   const [hideProfile, setHideProfile] = useState(true);
@@ -13,7 +11,7 @@ export const DropDownProfile = () => {
   const imgRef = useRef();
 
   const handleDrop = () => {
-    //console.log("hideProfile", hideProfile);
+    // console.log("hideProfile", hideProfile);
     setHideProfile((prev) => !prev);
   };
 
@@ -40,21 +38,19 @@ export const DropDownProfile = () => {
   }, []); // Abhängigkeiten array ist leer, da wir nur beim Mounten/Unmounten handeln wollen
 
   return (
-    <div className="btn-profile" onClick={handleDrop}>
-      <button ref={imgRef} onClick={handleDrop} className="btn-profile">
+    <div className="nav-items flex items-center gap-4" onClick={handleDrop}>
+      <button ref={imgRef}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-6 h-6 btn-profile"
-          onClick={handleDrop}
+          className="w-8 h-8 btn-profile"
         >
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            onClick={handleDrop}
             d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
           />
         </svg>
